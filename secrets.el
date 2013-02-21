9d10f31ec71d96466d735bba6d06647a26028e09
Olof-Joachim Frahm <olof@macrolet.net>
2013-02-21 22:55:05 +0100

    Handle decryption errors.

--- secrets.el
+++ secrets.el
@@ -1,4 +1,4 @@
 ;; i.e. http://emacs-fu.blogspot.de/2011/02/keeping-your-secrets-secret.html
 
-(when (load-library "secrets.el.gpg")
+(when (ignore-errors (load-library "secrets.el.gpg"))
   (provide 'secrets))
